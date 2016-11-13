f0bdb87afbfe27cfa78200d137be3cdaa52673bc
Li_jinzhou <414820128@qq.com>
2016-11-13 22:56:26 +0800

    日志系统ok

--- app/common/mylogger.py
+++ app/common/mylogger.py
@@ -7,7 +7,7 @@
 # Last Modified : 
 # Version       : 1.0
 # 
-# Description   : support base conf
+# Description   : log system
 #*****************************************************************************
 
 import logging
@@ -23,26 +23,30 @@ def get_path(name, parent=False):
     path_comp[-1] = name
     return os.sep.join(path_comp)
 
+
 def log_path():
     return get_path('log',parent=True)
 
-class MyLogger:
-
+# 使用方法见 main/test_log.py
+def log(model):
     # 创建一个logger
-    logger = logging.getLogger('mylogger')
+    logger = logging.getLogger(model)
+
+    # 日志级别 ：NOTSET DEBUG  INFO  WARNING  ERROR
     logger.setLevel(logging.DEBUG)
+
     # 创建一个handler，用于写入日志文件
-    fh = logging.FileHandler('test.log')
-    fh.setLevel(logging.DEBUG)
+    fh = logging.FileHandler(log_path()+os.sep+'test.log')
+    #fh.setLevel(logging.DEBUG)
     # 再创建一个handler，用于输出到控制台
     ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
+    #ch.setLevel(logging.DEBUG)
     # 定义handler的输出格式
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s %(name)s %(funcName)s %(lineno)d %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
     # 给logger添加handler
     logger.addHandler(fh)
     logger.addHandler(ch)
-    # 记录一条日志
-    logger.info('foorbar')
+
+    return logger
