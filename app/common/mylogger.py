2bd580be47055084fc85025d47d18e7fe8e6bcde
Li_jinzhou <414820128@qq.com>
2016-11-14 23:14:06 +0800

    logger

--- app/common/mylogger.py
+++ app/common/mylogger.py
@@ -41,14 +41,15 @@ def log(model):
 
     # 创建一个handler，用于写入日志文件
     fh = logging.FileHandler(log_path()+os.sep+'test.log')
-    #fh.setLevel(logging.DEBUG)
+    
     # 再创建一个handler，用于输出到控制台
     ch = logging.StreamHandler()
-    #ch.setLevel(logging.DEBUG)
+    
     # 定义handler的输出格式
     formatter = logging.Formatter('%(asctime)s %(name)s %(funcName)s %(lineno)d %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
+    
     # 给logger添加handler
     logger.addHandler(fh)
     logger.addHandler(ch)
